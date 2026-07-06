fcefe848f04beb84de55e1420bfa3b2e0adda5ef
AntonAzer <tonyazer86@gmail.com>
2026-07-06 08:00:47 +0300

    Delete name = "cv-backend" main = "src directory

--- name = "cv-backend" main = "src/src/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(req, env, ctx) {
-    return new Response(JSON.stringify({ status: "Backend شغال ✅" }), {
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-}
